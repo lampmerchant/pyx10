946ef18cc49b6ed170bc013ce90802b197acad60
lampmerchant <66847847+lampmerchant@users.noreply.github.com>
2023-07-28 11:41:06 -0600

    20230728-2

--- example.py
+++ example.py
@@ -4,6 +4,22 @@
 import pyx10
 
 
+def x10_d3_on(intf):
+  print('D3 is now on')
+
+
+def x10_d3_off(intf):
+  print('D3 is now off')
+
+
+def x10_d4_on(intf):
+  intf.get_controller('D').on(2)
+
+
+def x10_d4_off(intf):
+  intf.get_controller('D').off(2)
+
+
 def at_0700_mtwrf(intf):
   intf.get_controller('D').on(9)
 
